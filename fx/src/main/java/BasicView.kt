12f90febec491da99474b7b0af7bbb417f83940d
Dominic Jones <dojones@atlassian.com>
2017-06-30 15:44:43 +1000

    Simple controller

--- fx/src/main/java/BasicView.kt
+++ fx/src/main/java/BasicView.kt
@@ -3,16 +3,33 @@ import javafx.scene.control.Label
 import javafx.scene.layout.Pane
 import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
+import tornadofx.Controller
 import tornadofx.View
+import tornadofx.action
 import tornadofx.borderpane
+import tornadofx.button
 import tornadofx.plusAssign
 
 class BasicView : View() {
+
+    val controller: BasicController by inject()
 
     override val root: Pane = borderpane {
         top(TopView::class)
         left(LeftBar::class)
         center = Label("Testing")
+        bottom = button {
+            text = "doThing"
+            action {
+                controller.doThing()
+            }
+        }
+    }
+}
+
+class BasicController : Controller() {
+    fun doThing() {
+        println("blah")
     }
 }
 
